fca202053aef48eca0fabed837324aa1ba8eaa77
System Administrator <root@MYMACPRO.local>
2017-01-06 12:36:55 +0100

    postgres test db9

--- cache/data_ext.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1502793977
-184
-a:1:{s:20:"phpbbmodders/adduser";a:4:{s:8:"ext_name";s:20:"phpbbmodders/adduser";s:10:"ext_active";s:1:"1";s:9:"ext_state";s:4:"b:0;";s:8:"ext_path";s:25:"ext/phpbbmodders/adduser/";}}
