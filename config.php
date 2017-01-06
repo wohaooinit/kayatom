ac5059861794ff7b3d64e5cd4863a10a5af86ce4
System Administrator <root@MYMACPRO.local>
2017-01-06 10:57:39 +0100

    end install2

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+// phpBB 3.1.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\postgres';
+$dbhost = 'ec2-107-20-163-238.compute-1.amazonaws.com';
+$dbport = '5432';
+$dbname = 'd8r6r710fjdvhd';
+$dbuser = 'tndwbpredotanl';
+$dbpasswd = '751311b31e7d11462f823967d27664dff96ea124999b5a3566b8f9f53f62bd0d';
+$table_prefix = 'p_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+// @define('PHPBB_DISPLAY_LOAD_TIME', true);
+// @define('DEBUG', true);
+// @define('DEBUG_CONTAINER', true);
+
